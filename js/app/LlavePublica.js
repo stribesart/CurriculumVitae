e99e409832c976be170aaf32dc0b2e76443e223a
Set Reyes <set_abner@hotmail.com>
2021-06-25 03:15:56 -0500

    criptografiaDefinida

--- js/app/LlavePublica.js
+++ js/app/LlavePublica.js
@@ -1,18 +1,20 @@
-import { Boton } from "../componentes/Boton.js";
 import { MuestraLlavePublica } from "./MuestraLLavePublica.js";
 
 export function LlavePublica(){
   const contenido = document.getElementById("contenido");
   const contenedor = document.createElement("div");
-  
-  contenedor.appendChild(Boton({
-    "name": "llave",
-    "clases": "btn btn-ligth",
-    "texto": `<i class="fas fa-key"></i> Mi Llave Publica`,
-    "funcionClick": MuestraLlavePublica
-  }));
+
+  contenedor.innerHTML = MuestraLlavePublica();
 
   contenedor.className = "boton-llave"
   contenido.innerHTML = null;
   contenido.appendChild(contenedor);
+
+  const llavePublica = new JSEncrypt();
+  const pubk = document.getElementById("llavePublica");
+  
+  pubk.innerText = llavePublica.getPublicKey();
+  const privk = new Blob([llavePublica.getPrivateKey()], {type: "text/plain;charset=utf-8"});
+  // FileSaver
+  saveAs(privk, "LlavePrivada.txt");
 }
--- js/app/MuestraLLavePublica.js
+++ js/app/MuestraLLavePublica.js
@@ -1,3 +1,12 @@
 export function MuestraLlavePublica(){
-  console.log("hola")
+  return `
+    <div>
+      <div>
+        <textarea name="" id="llavePublica" cols="50" rows="10" disabled></textarea>
+      </div>
+      <button type="button" class="btn" id="redirecciona">
+        <a href="http://127.0.0.1:5500/index.html">Practica Firma Digital</a>
+      </button>
+    </div>
+  `;
 }
